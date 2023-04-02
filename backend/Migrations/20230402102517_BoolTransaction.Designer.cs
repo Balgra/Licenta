977d7fe76ee51b13db7796598c637d9907d93085
Balgra <andreibalgradean16@gmail.com>
2023-04-02 14:50:27 +0300

    Added more logic to how to handle transactions

--- backend/Migrations/20230402102517_BoolTransaction.Designer.cs
+++ backend/Migrations/20230402102517_BoolTransaction.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backend;
 
@@ -11,9 +12,11 @@ using backend;
 namespace backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230402102517_BoolTransaction")]
+    partial class BoolTransaction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -61,18 +64,6 @@ namespace backend.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<int?>("Cost_TierFour")
-                        .HasColumnType("int");
-
-                    b.Property<int?>("Cost_TierOne")
-                        .HasColumnType("int");
-
-                    b.Property<int?>("Cost_TierThree")
-                        .HasColumnType("int");
-
-                    b.Property<int?>("Cost_TierTwo")
-                        .HasColumnType("int");
 
                     b.Property<int>("OfferId")
                         .HasColumnType("int");
