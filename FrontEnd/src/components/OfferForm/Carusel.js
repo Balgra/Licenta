d00d7f3400aa7a3e997a0af9c85b81fd07f0f88d
Balgra <andreibalgradean16@gmail.com>
2023-06-05 16:54:17 +0300

    added forntend

--- FrontEnd/src/components/OfferForm/Carusel.js
+++ FrontEnd/src/components/OfferForm/Carusel.js
@@ -3,54 +3,51 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 
 const Carusel = () =>{
 	
-	return (<div id="carouselExampleFade" className="carousel slide carousel-fade">
-		<div className="carousel-inner">
-			<div className="carousel-item active">
-				<img
-					src="https://via.placeholder.com/800x400?text=Placeholder+Image+1"
-					className="d-block w-100"
-					alt="First slide"
-				/>
-				<div className="carousel-caption d-none d-md-block">
-					<h5>Second slide label</h5>
-					<p>Some representative placeholder content for the second slide.</p>
+	
+	return (
+		<div id="carouselExampleFade" className="carousel slide carousel-fade caru">
+			<div className="carousel-inner">
+				<div className="carousel-item active">
+					<img
+						src="https://via.placeholder.com/800x400?text=Placeholder+Image+1"
+						className="d-block w-100"
+						alt="First slide"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						src="https://via.placeholder.com/800x400?text=Placeholder+Image+2"
+						className="d-block w-100"
+						alt="Second slide"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						src="https://via.placeholder.com/800x400?text=Placeholder+Image+3"
+						className="d-block w-100"
+						alt="Third slide"
+					/>
 				</div>
 			</div>
-			<div className="carousel-item">
-				<img
-					src="https://via.placeholder.com/800x400?text=Placeholder+Image+2"
-					className="d-block w-100"
-					alt="Second slide"
-				/>
-				<div className="carousel-caption d-none d-md-block">
-					<h5>Second slide label</h5>
-					<p>Some representative placeholder content for the second slide.</p>
-					
-				</div>
-			</div>
-			<div className="carousel-item">
-				<img
-					src="https://via.placeholder.com/800x400?text=Placeholder+Image+3"
-					className="d-block w-100"
-					alt="Third slide"
-				/>
-				<div className="carousel-caption d-none d-md-block">
-					<h5>Second slide label</h5>
-					<p>Some representative placeholder content for the second slide.</p>
-				</div>
-			</div>
+			<button
+				className="carousel-control-prev"
+				type="button"
+				data-bs-target="#carouselExampleFade"
+				data-bs-slide="prev"
+			>
+				<span className="carousel-control-prev-icon" aria-hidden="true"/>
+				<span className="visually-hidden">Previous</span>
+			</button>
+			<button
+				className="carousel-control-next"
+				type="button"
+				data-bs-target="#carouselExampleFade"
+				data-bs-slide="next"
+			>
+				<span className="carousel-control-next-icon" aria-hidden="true"/>
+				<span className="visually-hidden">Next</span>
+			</button>
 		</div>
-		<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade"
-				data-bs-slide="prev">
-			<span className="carousel-control-prev-icon" aria-hidden="true"/>
-			<span className="visually-hidden">Previous</span>
-		</button>
-		<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade"
-				data-bs-slide="next">
-			<span className="carousel-control-next-icon" aria-hidden="true"/>
-			<span className="visually-hidden">Next</span>
-		</button>
-	</div>
 	);
 	
 }
