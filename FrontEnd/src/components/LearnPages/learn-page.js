5c2b69cf70dc762dc0937c54852a19ebefea7378
Balgra <andreibalgradean16@gmail.com>
2023-06-25 12:08:10 +0300

    Final touchings

--- FrontEnd/src/components/LearnPages/learn-page.js
+++ FrontEnd/src/components/LearnPages/learn-page.js
@@ -3,18 +3,16 @@ import './learn-page.css';
 
 const Learn = () => {
 	return (
-		<div className="learn-container">
-			<div className="app__wrapper_info section__padding">
-				<h1 className="headteacher__cormorant">More Learning</h1>
-				<p className="app_color">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet mi eu libero posuere facilisis. Integer
-					posuere tellus eu sem scelerisque faucibus. Etiam nec fermentum nisl, eget commodo enim. Aliquam consectetur sit
-					amet elit quis ultricies.
-				</p>
+		<div className="learn-container bg-light">
+			<div className="app__wrapper_info section__padding bg-light">
+				<h1 className="headteacher__cormorant text-black text-center">Welcome to BizBoost Learning Page</h1>
+				<h5 className=" text-black">
+					This page is meant for you. Do you want to make great businesses, or invest in the next big company? Here is your place to learn how to do it.
+				</h5>
 			</div>
 			
-			<div className="app__wrapper">
-				<div className="app__wrapper_videos section__padding">
+			<div className="app__wrapper bg-light">
+				<div className="app__wrapper_videos section__padding bg-light">
 					<div className="row">
 						<div className="col-md-6 mb-4">
 							<iframe className="responsive-iframe" src="https://www.youtube.com/embed/gNRGkMeITVU"
@@ -51,36 +49,18 @@ const Learn = () => {
 							/>
 						</div>
 					</div>
-					{/* Add more video elements here */}
-					<p className="app_color">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet mi eu libero posuere facilisis.
-						Integer posuere tellus eu sem scelerisque faucibus. Etiam nec fermentum nisl, eget commodo enim. Aliquam
-						consectetur sit amet elit quis ultricies.
-					</p>
-					<p className="app_color">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet mi eu libero posuere facilisis.
-						Integer posuere tellus eu sem scelerisque faucibus. Etiam nec fermentum nisl, eget commodo enim. Aliquam
-						consectetur sit amet elit quis ultricies.
-					</p>
-					<p className="app_color">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet mi eu libero posuere facilisis.
-						Integer posuere tellus eu sem scelerisque faucibus. Etiam nec fermentum nisl, eget commodo enim. Aliquam
-						consectetur sit amet elit quis ultricies.
-					</p>
 				</div>
 			</div>
 			
-			<div className="app__wrapper_info section__padding">
-				<h1 className="headteacher__cormorant">More Learning</h1>
-				<p className="app_color">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet mi eu libero posuere facilisis. Integer
-					posuere tellus eu sem scelerisque faucibus. Etiam nec fermentum nisl, eget commodo enim. Aliquam consectetur sit
-					amet elit quis ultricies.
-				</p>
+			<div className="app__wrapper_info section__padding bg-light">
+				<h1 className="headteacher__cormorant text-black bg-light">How about learning how to build your own business</h1>
+				<h5 className=" text-black">
+					Here are some videos you can start with:
+				</h5>
 			</div>
 			
-			<div className="app__wrapper">
-				<div className="app__wrapper_videos section__padding">
+			<div className="app__wrapper bg-light">
+				<div className="app__wrapper_videos section__padding bg-light">
 					<div className="row">
 						<div className="col-md-6 mb-4">
 							<iframe className="responsive-iframe" src="https://www.youtube.com/embed/DAvGB6UWbQ4"
@@ -117,88 +97,42 @@ const Learn = () => {
 							/>
 						</div>
 					</div>
-					{/* Add more video elements here */}
-					<p className="app_color">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet mi eu libero posuere facilisis.
-						Integer posuere tellus eu sem scelerisque faucibus. Etiam nec fermentum nisl, eget commodo enim. Aliquam
-						consectetur sit amet elit quis ultricies.
-					</p>
-					<p className="app_color">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet mi eu libero posuere facilisis.
-						Integer posuere tellus eu sem scelerisque faucibus. Etiam nec fermentum nisl, eget commodo enim. Aliquam
-						consectetur sit amet elit quis ultricies.
-					</p>
-					<p className="app_color">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet mi eu libero posuere facilisis.
-						Integer posuere tellus eu sem scelerisque faucibus. Etiam nec fermentum nisl, eget commodo enim. Aliquam
-						consectetur sit amet elit quis ultricies.
-					</p>
 				</div>
 			</div>
-			<div className="d-flex flex-wrap flex__center">
-			<ul className="list-group list-group-horizontal">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-			</ul>
-			<ul className="list-group list-group-horizontal-sm">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-			</ul>
-			<ul className="list-group list-group-horizontal-md">
-				<li className="list-group-item">Aaaaan item</li>
-				<li className="list-group-item">Aaaaaaa second item</li>
-				<li className="list-group-item">Aaaaa third item</li>
-			</ul>
-			<ul className="list-group list-group-horizontal-lg">
-				<li className="list-group-item">Aaan item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-			</ul>
-			<ul className="list-group list-group-horizontal-xl">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-			</ul>
-			<ul className="list-group list-group-horizontal-xxl">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-			</ul>
-			<ul className="list-group list-group-horizontal">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-			</ul>
-			<ul className="list-group list-group-horizontal-sm">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-			</ul>
-			<ul className="list-group list-group-horizontal-md">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-			</ul>
-			<ul className="list-group list-group-horizontal-lg">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-			</ul>
-			<ul className="list-group list-group-horizontal-xl">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-			</ul>
-			<ul className="list-group list-group-horizontal-xxl">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-			</ul>
+			<div className="d-flex align-items-center justify-content-center bg-light">
+				<div className="text-center">
+					<div className="d-flex">
+						<ul className="list-group  list-group-lg me-3 border border-dark">
+							<li className="list-group-item border border-dark"><strong>Books about investing</strong></li>
+							<li className="list-group-item border border-dark">"The Intelligent Investor" by Benjamin Graham</li>
+							<li className="list-group-item border border-dark">"Common Stocks and Uncommon Profits" by Philip Fisher</li>
+							<li className="list-group-item border border-dark">"A Random Walk Down Wall Street" by Burton Malkiel:</li>
+							<li className="list-group-item border border-dark">"The Little Book of Common Sense Investing"  by John C. Bogle</li>
+							<li className="list-group-item border border-dark">"Thinking, Fast and Slow" by Daniel Kahneman</li>
+						</ul>
+						<ul className="list-group list-group-lg ms-3">
+							<li className="list-group-item border border-dark"><strong>Books about making a business</strong></li>
+							<li className="list-group-item border border-dark">"The Lean Startup" by Eric Ries</li>
+							<li className="list-group-item border border-dark">"The E-Myth Revisited" by Michael E. Gerber</li>
+							<li className="list-group-item border border-dark">"Zero to One" by Peter Thiel</li>
+							<li className="list-group-item border border-dark">"The Innovator's Dilemma" by Clayton M. Christensen</li>
+							<li className="list-group-item border border-dark">"Start with Why" by Simon Sinek</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 			<br/>
 		    <br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
 			<br/>
 		</div>
 	);
